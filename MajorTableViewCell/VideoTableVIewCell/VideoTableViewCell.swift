1b4a564a3e06a1cd8398cd9f67c090ff73137445
p-x9 <chi.iosdev@gmail.com>
2019-09-29 22:03:13 +0900

    VideoViewCellを作成しました

---
+++ MajorTableViewCell/VideoTableVIewCell/VideoTableViewCell.swift
@@ -0,0 +1,56 @@
+//
+//  VideoTableViewCell.swift
+//  MajorTableViewCell
+//
+//  Created by p-x9 on 2019/09/29.
+//  Copyright © 2019 p-x9. All rights reserved.
+//
+
+import UIKit
+
+//@IBDesignable
+public class VideoTableViewCell: UITableViewCell {
+    
+    @IBOutlet public var thumbnailView: UIImageView!
+    @IBOutlet public var titleLabel: UILabel!
+    @IBOutlet public var detailLabel: UILabel!
+    
+    
+    
+    var cellview: UIView!
+    
+    /*override public init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        initView()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initView()
+        fatalError("init(coder:) has not been implemented")
+    }*/
+    
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        initView()
+    }
+    
+    private func initView(){
+        let nib = UINib(nibName: "VideoTableViewCell", bundle: Bundle(for: VideoTableViewCell.self))
+        
+        cellview = nib.instantiate(withOwner: self, options: nil).first as? UIView
+        addSubview(cellview)
+        cellview.point(inside: CGPoint(x: 0, y: 0), with: nil)
+        titleLabel.text = ""
+        detailLabel.text = ""
+    }
+
+    override public func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
