372f654f28e2e062d0748ee32f0fa516eb90a523
p-x9 <chi.iosdev@gmail.com>
2019-09-29 14:07:51 +0900

    Initial Commit

---
+++ MajorTableViewCell/MajorTableViewCell.h
@@ -0,0 +1,19 @@
+//
+//  MajorTableViewCell.h
+//  MajorTableViewCell
+//
+//  Created by p-x9 on 2019/09/29.
+//  Copyright © 2019 p-x9. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MajorTableViewCell.
+FOUNDATION_EXPORT double MajorTableViewCellVersionNumber;
+
+//! Project version string for MajorTableViewCell.
+FOUNDATION_EXPORT const unsigned char MajorTableViewCellVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MajorTableViewCell/PublicHeader.h>
+
+
